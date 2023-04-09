55d7109da9e43cf6665d3b15a8e30f47f67ec8d3
Andrew Lee <andrewzhlee@gmail.com>
2023-04-09 06:41:44 -0400

    somewhat working version

---
+++ README.md
@@ -0,0 +1,13 @@
+## Bitcamp 2023 Hackathon Submission
+
+Challenge:
+
+Fannie Mae - Don’t Put All Your Eggs in One Basket (2)
+
+Employing classic algorithms to create high-value collateral pools from mortgages
+
+Our challenge to you: Utilize a knapsack algorithm to fill a knapsack with objects without exceeding the knapsack’s weight capacity. In this challenge, the knapsack is a pool of mortgages, the weight capacity is a monetary value (we’ll supply that at the start of the challenge), the objects are mortgages, the weight of a mortgage is its unpaid principal balance, and the value of a mortgage is determined by a number of other attributes associated with the mortgage.
+
+We supply the data (loans, knapsack “capacity”). You supply the ingenuity, the solution infrastructure, and the implementation approach.
+
+Your solution to the challenge will be evaluated based on completeness, innovation, and performance/throughput.
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
 import csv
 import pandas as pd
+import numpy as np
 
 MULTIPLIER = 1000000
+
+file_idx = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 
 def gen_datastructure():
@@ -52,7 +55,22 @@ def gen_datastructure():
         json.dump(categories, file)
 
 
+def find_max_pos(batches, threshold=None):
+    # print(batches)
+    pos = (0, 0)
+
+    for r in range(len(batches)):
+        for c in range(len(batches[r])):
+            if batches[r][c][1] > batches[pos[0]][pos[1]][1] and threshold and batches[r][c][1] < threshold:
+                r = r
+                c = c
+
+    return pos
+
+
 def knapsack():
+    global file_idx
+
     categories = {}
 
     pools = []
@@ -61,6 +79,7 @@ def knapsack():
     min_state = [20, 20, 10, 20, 20, 7, 20, 20, 20, 4]
     state_pct = [0.05, 0.05, 0.1, 0.05, 0.05, 0.15, 0.05, 0.05, 0.05, 0.25]
 
+    # with open('smalltest-copy copy copy.json', 'r') as file:
     with open('categories.json', 'r') as file:
         categories = json.load(file)
 
@@ -115,45 +134,175 @@ def knapsack():
             states = {}  # dict from state to list of loans
 
             # For a loan in a category, v, of (classes, maturity_date, and loan_term)
+
+            # state => MD: ([used loans], [unused loans])
             for loan in v:
                 if states.get(loan[6]) is None:
-                    states[loan[6]] = [loan]
+                    states[loan[6]] = ([], [loan])
                 else:
-                    states[loan[6]].append(loan)
+                    states[loan[6]][1].append(loan)
 
-            min_state_loans = 100000
+            # Sort the loans in each state's unused loans
+            for _, tup in states.items():
+                tup[1].sort(key=lambda x: x[1], reverse=True)
+
+            #
+            # start new algorithm:
+            #
+
+            # if # states < threhold -> skip
             if len(states.keys()) < min_state[pclass-1]:
                 continue
 
-            for state in states.keys():
-                # sort loans by loan amount
-                states[state].sort(key=lambda x: x[1], reverse=True)
-                if len(states[state]) < min_state_loans:
-                    min_state_loans = len(states[state])
+            # make first batch (will work for sure b/c of if above)
+            batches = [[[]]]
+            batch_bals = [0]
+            for _, tup in states.items():
+                if len(tup[1]) > 0:
+                    first = tup[1].pop(0)
+                    tup[0].append(first)
+                    batches[-1][0].append(first)
+                    batch_bals[0] += first[1]
 
-            while min_state_loans > 0 and pool[2] < max_size[pclass-1] * MULTIPLIER:
-                sum = 0
-                for state in states.keys():
-                    if len(states[state]) > 0:
-                        sum += states[state][0][1]
-                # TODO: improve --> maybe just pop --> knapsack?
+            # check if batch < max_size
+            while batch_bals[-1] < max_size[pclass-1] * MULTIPLIER:
+                # Initialize new batch
+                prev = batches[-1]
+                prev.append([])
+                batches.append(prev)
+                batch_bals.append(batch_bals[-1])  # initializes to prev balance
 
-                if pool[2] + sum < max_size[pclass-1] * MULTIPLIER:
-                    for state in states.keys():
-                        if len(states[state]) > 0:
-                            pool[2] += states[state][0][1]
-                            pool[3][state] = pool[3].get(state, 0) + 1
-                            pool[4].append(states[state][0][:-1])
-                            states[state].pop(0)
-                            
-                min_state_loans -= 1
+                # add another batch
+                for _, tup in states.items():
+                    if len(tup[1]) > 0:
+                        first = tup[1].pop(0)
+                        tup[0].append(first)
+                        batches[-1][-1].append(first)
+                        batch_bals[-1] += first[1]
+
+                # checks new batch state percentage is still valid (exit case)
+                if len(batches[-1][-2]) == min_state[pclass-1] and len(batches[-1][-1]) < min_state[pclass-1]:
+                    # batch[-2] is the solution
+                    batch_bals.pop()
+                    if len(batches[-1][-1]) > 0:
+                        batches[-1][-1].pop()  # just left with batches[:-2]
+                    else:
+                        batches[-1].pop()
+                    break
+
+                if len(batches[-1][-1]) == 0:  # (exit case)
+                    batch_bals.pop()
+                    batches[-1].pop()
+                    break
+
+            # Replacement alg if balance greater than threshold
+            prev_max = float('inf')
+            num_states = len(batches[0])
+            tested_states = 0
+            while batch_bals[-1] > max_size[pclass-1] * MULTIPLIER and tested_states < num_states:
+                # find max of maxs
+                max_r, max_c = find_max_pos(batches[-1], prev_max)
+                max_state = batches[-1][max_r][max_c][6]
+                prev_max = batches[-1][max_r][max_c][1]
+
+                #  replace max value with next best
+                if len(states[max_state][1]) > 0:
+                    temp = batches[-1][max_r][max_c]
+                    batches[-1][max_r][max_c] = states[max_state][1][0]
+                    states[max_state][1][0] = temp
+                    # update balance
+                    batch_bals[-1] += batches[-1][max_r][max_c][1] - temp[1]
+
+                tested_states += 1
+
+            # Removal alg if balance still greater than threshold
+            prev_max = float('inf')
+            while batch_bals[-1] > max_size[pclass-1] * MULTIPLIER:
+                # find max of maxs
+                max_r, max_c = find_max_pos(batches[-1], prev_max)
+                max_state = batches[-1][max_r][max_c][6]
+                states[max_state][1].insert(0, batches[-1][max_r][max_c])
+                prev_max = batches[-1][max_r][max_c][1]
+                # update balance
+                batch_bals[-1] -= batches[-1][max_r][max_c][1]
+                #  remove max value
+                batches[-1][max_r].pop(max_c)
+
+            # check if soln still adheres to state percentage requirement
+            num_loans = 0
+            for batch in batches[-1]:
+                num_loans += len(batch)
+
+            for _, tup in states.items():
+                if len(tup[0])/num_loans > state_pct[pclass-1]:
+                    # state percentage req not met, revert to previous batch/soln
+                    batches.pop()
+                    break
+
+            #
+            # End new algorithm
+            #
+
+            soln = []
+            for b in batches[-1]:
+                soln += b
+            pool[-1] = soln
+            pools.append(pool)
+
+            # if batch_bal < max_size[pclass-1] * MULTIPLIER:
+            #     # add  another batch
+            # else:
+            #     # run replacement algorithm
+
+            # while min_state_loans > 0 and pool[2] < max_size[pclass-1] * MULTIPLIER:
+            #     sum = 0
+            #     for state in states.keys():
+            #         if len(states[state]) > 0:
+            #             sum += states[state][0][1]
+
+            #     if pool[2] + sum < max_size[pclass-1] * MULTIPLIER:
+            #         for state in states.keys():
+            #             if len(states[state]) > 0:
+            #                 pool[2] += states[state][0][1]
+            #                 pool[3][state] = pool[3].get(state, 0) + 1
+            #                 pool[4].append(states[state][0][:-1])
+            #                 states[state].pop(0)
+
+            #     min_state_loans -= 1
+
+            # end new algorihtm:
+
+            # min_state_loans = 100000
+            # if len(states.keys()) < min_state[pclass-1]:
+            #     continue
+
+            # for state in states.keys():
+            #     # sort loans by loan amount
+            #     states[state].sort(key=lambda x: x[1], reverse=True)
+            #     if len(states[state]) < min_state_loans:
+            #         min_state_loans = len(states[state])
+
+            # while min_state_loans > 0 and pool[2] < max_size[pclass-1] * MULTIPLIER:
+            #     sum = 0
+            #     for state in states.keys():
+            #         if len(states[state]) > 0:
+            #             sum += states[state][0][1]
+            #     # TODO: improve --> maybe just pop --> knapsack?
+
+            #     if pool[2] + sum < max_size[pclass-1] * MULTIPLIER:
+            #         for state in states.keys():
+            #             if len(states[state]) > 0:
+            #                 pool[2] += states[state][0][1]
+            #                 pool[3][state] = pool[3].get(state, 0) + 1
+            #                 pool[4].append(states[state][0][:-1])
+            #                 states[state].pop(0)
+
+            #     min_state_loans -= 1
 
             # if len(pool[4]) >= 20:
             #     pools.append(pool)
             # else:
             #     print(pool[4])
-
-            pools.append(pool)
 
             # pool size check
             # if pool[2] + loan[1] <= max_size[pclass-1] * MULTIPLIER:
@@ -189,18 +338,17 @@ def knapsack():
     #     json.dump(soln, file)
 
     # follow output guidelines
-    count = 0
     for pool in pools:
-        # num_loans = len(pool[4])
-
-        with open(f"output/class{pool[0]}_{count}.txt", "w", encoding="UTF-8") as file:
+        print(pool[4])
+        count = file_idx[pool[0]-1]
+        file_idx[pool[0]-1] += 1
+        with open(f"output/pool-{pool[0]}-{count}.txt", "w", encoding="UTF-8") as file:
             writer = csv.writer(file, delimiter='|')
             writer.writerow(["loan_id", "upb", "note_rate", "borrower_fico", "coborrower_fico",
                             "combined_fico", "state", "dti", "ltv", "maturity_date", "loan_term", "property_type"])
             for ele in pool[4]:
                 del ele[-1]
             writer.writerows(pool[4])
-        count += 1
 
     # optimize categories:
     # flag = False
